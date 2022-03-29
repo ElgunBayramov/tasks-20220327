cba6fc7cc551d250c42f0195a884da945bdd2fbf
Elguncode <elgunmb@code.edu.az>
2022-03-29 21:07:58 +0400

    tapsiriq

--- task3/Program.cs
+++ task3/Program.cs
@@ -8,8 +8,7 @@ namespace task3
         {
             // verilmihs 9 reqemli ededin duz ortaya dushen 3 reqeminin reqemleri cemi.
             Console.WriteLine("ededi daxil et: ");
-            int eded=int.Parse(Console.ReadLine());
-            if(eded>100000000 && eded>=)
+            
         }
     }
 }
