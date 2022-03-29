091282119e1a50f5f0b82b4861073df9a20db6bb
Elguncode <elgunmb@code.edu.az>
2022-03-29 23:25:59 +0400

    tapsiriq

--- task3/Program.cs
+++ task3/Program.cs
@@ -7,8 +7,29 @@ namespace task3
         static void Main(string[] args)
         {
             // verilmihs 9 reqemli ededin duz ortaya dushen 3 reqeminin reqemleri cemi.
+            int sum = 0;
+            int sum1 = 0;
+            int sum2 = 0;
             Console.WriteLine("ededi daxil et: ");
-            
+            int eded=int.Parse(Console.ReadLine());
+            if(eded>=100000000 && eded < 1000000000)
+            {
+                while (eded > 0)
+                {
+                    sum1 = sum1 + (eded / 1000000) % 10;
+                    sum2 = sum2 + (eded / 1000) % 10;
+                    eded /= 10;
+
+                }
+                Console.WriteLine($"ededin ilk 3 reqeminin cemi: {sum1}");
+                Console.WriteLine($"ededin ilk 6 reqeminin cemi: {sum2}");
+                sum=sum2- sum1;
+                Console.WriteLine($"ededin ortaya dushen 3 reqeminin cemi: {sum}");
+            }
+            else
+            {
+                Console.WriteLine("sert duzgun odenilmeyib");
+            }
         }
     }
 }
