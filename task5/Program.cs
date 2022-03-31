64265b4631da56f97a31e65ec41059b4b0a9ecb9
Elguncode <elgunmb@code.edu.az>
2022-03-31 15:40:30 +0400

    tapsiriq

--- task5/Program.cs
+++ task5/Program.cs
@@ -9,12 +9,15 @@ namespace task5
             // verilmihs 6 reqemli ededin 1 ci reqemini hemin ededin axirina at.
            
             int a1;
+            int b;
             Console.Write("eded daxil edin: ");
             int eded=int.Parse(Console.ReadLine());
             if(eded>=100000 && eded < 1000000)
             {
                 a1 = eded / 100000;
-                Console.WriteLine($"{eded}{a1}");
+                Console.WriteLine($"{eded} ededinin birinci reqemi: {a1}");
+                b = eded * 10 + a1;
+                Console.WriteLine($"{eded} ededinin 1-ci reqemini ededin axirina atdiqda alinan eded: {b}");
             }
             else
             {
